9c2c750d7c102b8fe9ffc270514fba472fff41a2
jemin-surefy <anildervaliya20@gmail.com>
2024-10-18 10:48:51 +0530

    v0101
    addnew View

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.mylibrary.Snacky
 import com.example.mylibrary.testmodelactiity
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.tvid)
+        val layout = findViewById<ConstraintLayout>(R.id.lay_View)
+        Snacky.showSnackbar(layout, "This is a test message!!")
 
         button.setOnClickListener {
             val intent = Intent(this, testmodelactiity::class.java)
